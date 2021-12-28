4b00bc522024925955968f3b8564d6506c030146
Vitalii Slobodoniuk <slvitalik24@ukr.net>
2021-12-28 15:37:24 +0200

    task done

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log('------------- # 4');
+
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+delay(1000).then(() => console.log('Hello'));
+
+console.log('------------- # 5');
+
+function getUserInfo() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 }), 1000);
+  });
+}
+
+function getUserAvatar(userInfo) {
+  return new Promise(function (resolve, reject) {
+    userInfo.avatar =
+      'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg';
+    setTimeout(() => resolve(userInfo), 1000);
+  });
+}
+
+function getUserAdditionalInfo(userInfo) {
+  return new Promise(function (resolve, reject) {
+    userInfo.interests = ['sport', 'books'];
+    setTimeout(() => resolve(userInfo), 1000);
+  });
+}
+
+getUserInfo()
+  .then((res) => getUserAvatar(res))
+  .then((res) => getUserAdditionalInfo(res))
+  .then((res) => console.log(res));
+
+console.log('------------- # 6');
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 }), 1000);
+})
+  .then(function (userInfo) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(() => reject(new Error('wrong data')), 1000);
+    });
+  })
+  .catch((error) => console.log(error));
